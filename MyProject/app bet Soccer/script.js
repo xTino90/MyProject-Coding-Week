f1e5b369ed7cdf55f329140266e871e7673bc469
Tino Di Costanzo <tino.dicostanzo90@gmail.com>
2023-12-24 11:55:31 +0100

    upload exercise

--- MyProject/app bet Soccer/script.js
+++ MyProject/app bet Soccer/script.js
@@ -144,3 +144,71 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamstr = team === "x" ? "draw" : `victory ${game[team]}`;
   console.log(`Odd of ${teamstr} ${odd}`);
 }
+
+const array1 = [1, 2, 3, 4, 5];
+const array = [...new Set(array1)];
+
+// console.log(array.has(2));
+console.log(array.size);
+console.log(array);
+
+// challenge 3.
+
+/*1. Create an array 'events' of the different game events that happened (no 
+  duplicates)
+
+  2. After the game has finished, is was found that the yellow card from minute 64 
+  was unfair. So remove this event from the game events log.
+
+  3. Compute and log the following string to the console: "An event happened, on 
+  average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+  4. Loop over 'gameEvents' and log each element to the console, marking 
+  whether it's in the first half or second half (after 45 min) of the game, like this:
+  [FIRST HALF] 17: ⚽ GOAL*/
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// 2.
+
+gameEvents.delete(64);
+console.log([...gameEvents.entries()]);
+
+// 3.
+// Extract event times from the map
+const eventTimes = Array.from(gameEvents.keys());
+
+// Calculate the average time difference between events
+const totalMinutes = 90; // Total duration of the game
+const averageTimeDifference = totalMinutes / (eventTimes.length - 1);
+
+// Construct the string
+const resultString = `An event happened, on average, every ${averageTimeDifference.toFixed(
+  2
+)} minutes`;
+
+// Log the result to the console
+console.log(resultString);
+
+// 4.
+const totalMinutes1 = 90;
+
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? "FIRST HALF" : "SECOND HALF";
+  console.log(`[${half}] ${minute}: ${event}`);
+}
